7634eb8e0b5124391231aa1e6a8be73284580940
Your Nameraviealla <your-email@example.comravi.unix89@gmail.com>
2024-08-26 14:02:21 +0530

    code

---
+++ src/main/java/myapp/com/pl/PlApplication.java
@@ -0,0 +1,13 @@
+package myapp.com.pl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myapp/com/pl/PlApplicationTests.java
@@ -0,0 +1,13 @@
+package myapp.com.pl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
